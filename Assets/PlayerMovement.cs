a4b1ba049675269cc38ca8b4dfea525eb3a397a0
elugo1 <82634063+elugo1@users.noreply.github.com>
2024-04-22 21:16:56 -0400

    added sprites and small behavior
    in this push i add the cage sprites, tires, tire rotations and started following a tutorial in 2d movement scripts that will be deleted later

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.XR;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInp = Input.GetAxis("Horizontal");
+        float yInp = Input.GetAxis("Vertical");
+
+
+        if(Mathf.Abs(xInp)>0){
+            body.velocity = new Vector2(xInp *speed, body.velocity.y);
+        }
+        if (Mathf.Abs(yInp)>0){
+            body.velocity = new Vector2(body.velocity.x,yInp *speed);
+        }
+
+    }
+}
