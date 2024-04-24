c8292efbfcb39098dac12f288b7b41fda827b44e
elugo1 <82634063+elugo1@users.noreply.github.com>
2024-04-24 01:18:24 -0400

    containers,container movement, speed changes
    I added containers, inverse movement for player 1 and player 2 containers, some slight file managment and key mapping

---
+++ Assets/Scripts/Container_move.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Container_move : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInp = Input.GetAxis("Horizontal");
+        float yInp = Input.GetAxis("Vertical");
+
+        // Modify the horizontal velocity based on vertical input
+        body.velocity = new Vector2(yInp * speed,xInp*speed);    
+        
+        }
+}
---
+++ Assets/Scripts/Container_move2.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Container_move2 : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInp = Input.GetAxis("Horizontal1");
+        float yInp = Input.GetAxis("Vertical1");
+
+        // Modify the horizontal velocity based on vertical input
+        body.velocity = new Vector2(yInp * speed,xInp*speed);    
+    }
+}
